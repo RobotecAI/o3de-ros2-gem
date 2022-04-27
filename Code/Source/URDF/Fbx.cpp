41e5897bbd85199e036158f30076879aee7f339a
Michal Drwiega <michal.drwiega@robotec.ai>
2022-04-27 19:43:15 +0200

    Fixed bug with double conversion in fbx
    Signed-off-by: Michal Drwiega <michal.drwiega@robotec.ai>

--- Code/Source/URDF/Fbx.cpp
+++ Code/Source/URDF/Fbx.cpp
@@ -33,6 +33,10 @@ namespace ROS2
         {
             return std::to_string(std::any_cast<float>(a));
         }
+        else if (a.type() == typeid(double))
+        {
+            return std::to_string(std::any_cast<double>(a));
+        }
         else if (a.type() == typeid(const char*))
         {
             return std::string("\"") + std::any_cast<const char*>(a) + std::string("\"");
