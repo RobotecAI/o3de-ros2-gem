246b61a037e9f6e61c8bf290c0c55e4d8a231107
Adam Dabrowski <adam.dabrowski@robotec.ai>
2022-09-08 16:52:23 +0200

    added reflections, headers
    Signed-off-by: Adam Dabrowski <adam.dabrowski@robotec.ai>

---
+++ Code/Source/VehicleDynamics/WheelControllerComponent.cpp
@@ -0,0 +1,68 @@
+/*
+ * Copyright (c) Contributors to the Open 3D Engine Project.
+ * For complete copyright and license terms please see the LICENSE at the root of this distribution.
+ *
+ * SPDX-License-Identifier: Apache-2.0 OR MIT
+ *
+ */
+
+#include "VehicleDynamics/WheelControllerComponent.h"
+#include <AzCore/Debug/Trace.h>
+#include <AzCore/Serialization/EditContext.h>
+#include <AzCore/Serialization/EditContextConstants.inl>
+#include <AzCore/Serialization/SerializeContext.h>
+
+namespace VehicleDynamics
+{
+    void WheelControllerComponent::Activate()
+    {
+    }
+
+    void WheelControllerComponent::Deactivate()
+    {
+    }
+
+    void WheelControllerComponent::GetRequiredServices(AZ::ComponentDescriptor::DependencyArrayType& required)
+    {
+        required.push_back(AZ_CRC("RigidBody"));
+    }
+
+    void WheelControllerComponent::GetProvidedServices(AZ::ComponentDescriptor::DependencyArrayType& provided);
+    {
+        provided.push_back(AZ_CRC("WheelController"));
+    }
+
+    void WheelControllerComponent::GetIncompatibleServices(AZ::ComponentDescriptor::DependencyArrayType& incompatible);
+    {
+        // Only one per entity
+        incompatible.push_back(AZ_CRC("WheelController"));
+    }
+
+    void WheelControllerComponent::Reflect(AZ::ReflectContext* context)
+    {
+        if (AZ::SerializeContext* serialize = azrtti_cast<AZ::SerializeContext*>(context))
+        {
+            serialize->Class<WheelControllerComponent, AZ::Component>()
+                ->Version(1)
+                ->Field("SteeringEntity", &WheelControllerComponent::m_steeringEntity)
+                ->Field("VisualEntity", &WheelControllerComponent::m_visualEntity);
+
+            if (AZ::EditContext* ec = serialize->GetEditContext())
+            {
+                ec->Class<WheelControllerComponent>("Wheel controller", "Handle wheel physics")
+                    ->ClassElement(AZ::Edit::ClassElements::EditorData, "")
+                    ->Attribute(AZ::Edit::Attributes::AppearsInAddComponentMenu, AZ_CRC("Game")) // TODO - "Simulation"?
+                    ->DataElement(
+                        AZ::Edit::UIHandlers::Default,
+                        &WheelControllerComponent::m_steeringEntity,
+                        "Steering entity",
+                        "Entity which steers the wheel - typically a parent entity")
+                    ->DataElement(
+                        AZ::Edit::UIHandlers::Default,
+                        &WheelControllerComponent::m_visualEntity,
+                        "Visual entity",
+                        "Entity which holds wheel visual (mesh) - typically a child entity");
+            }
+        }
+    }
+} // namespace VehicleDynamics
