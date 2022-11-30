RobotecAI/o3de-ros2-gem

commit d6b7701b47ccca4c31e28481ed70a61906068dcf

Introduced `MotorizedJointRequestBus` (#334)
* Introduced motorized `MotorizedJointRequestBus`

Signed-off-by: Michał Pełka <michalpelka@gmail.com>

Code/Source/Manipulator/MotorizedJointComponent.cpp
@@ -23,11 +23,13 @@ namespace ROS2
         {
             AZ::TransformBus::EventResult(m_debugDrawEntityInitialTransform, this->GetEntityId(), &AZ::TransformBus::Events::GetLocalTM);
         }
+        MotorizedJointRequestBus::Handler::BusConnect(m_entity->GetId());
     }
 
     void MotorizedJointComponent::Deactivate()
     {
         AZ::TickBus::Handler::BusDisconnect();
+        MotorizedJointRequestBus::Handler::BusDisconnect();
     }
 
     void MotorizedJointComponent::Reflect(AZ::ReflectContext* context)
@@ -248,12 +250,17 @@ namespace ROS2
         m_setpoint = setpoint;
     }
 
-    float MotorizedJointComponent::GetError() const
+    float MotorizedJointComponent::GetSetpoint()
+    {
+        return m_setpoint;
+    }
+
+    float MotorizedJointComponent::GetError()
     {
         return m_error;
     }
 
-    float MotorizedJointComponent::GetCurrentPosition() const
+    float MotorizedJointComponent::GetCurrentMeasurement()
     {
         return m_currentPosition - m_zeroOffset;
     }

Code/Include/ROS2/Manipulator/MotorizedJointBus.h
@@ -1,0 +1,57 @@
+/*
+ * Copyright (c) Contributors to the Open 3D Engine Project.
+ * For complete copyright and license terms please see the LICENSE at the root of this distribution.
+ *
+ * SPDX-License-Identifier: Apache-2.0 OR MIT
+ *
+ */
+#pragma once
+
+#include <AzCore/Component/EntityId.h>
+#include <AzCore/EBus/EBus.h>
+#include <AzCore/Interface/Interface.h>
+
+namespace ROS2
+{
+    //! Interface to communicate with motorized joints.
+    //! It allows to apply setpoint and tracking performance of the PID controller.
+    class MotorizedJointRequest : public AZ::EBusTraits
+    {
+    public:
+        static constexpr AZ::EBusAddressPolicy AddressPolicy = AZ::EBusAddressPolicy::ById;
+        using BusIdType = AZ::EntityId;
+
+        virtual ~MotorizedJointRequest() = default;
+
+        //! Set current setpoint value for position controller.
+        //! The setpoint is the desired position for a simulated actuator.
+        //! For linear actuators, the setpoint is given in meters.
+        //! For the rotational actuators, the setpoint is given in radians.
+        //! @param setpoint value in meters or radians
+        virtual void SetSetpoint(float setpoint) = 0;
+
+        //! Get current setpoint value for position controller.
+        //! If `SetSetpoint` has not been called, it returns initial position of joint.
+        //! For linear actuators, the setpoint is given in meters.
+        //! For the rotational actuators, the setpoint is given in radians.
+        //! @returns setpoint value in meters or radians
+        virtual float GetSetpoint() = 0;
+
+        //! Retrieve current measurement.
+        //! Measurement is the value of the movement - e.g. protrusion of an actuator.
+        //! For linear actuators, measurement is given in meters.
+        //! For the rotational actuators, measurement is given in radians.
+        //! When the setpoint is reached this should be close to setpoint.
+        //! @returns measurement value in meters or radians.
+        virtual float GetCurrentMeasurement() = 0;
+
+        //! Retrieve current control error (difference between setpoint and measurement)
+        //! When the setpoint is reached this should be close to zero.
+        //! For linear actuators, the error is given in meters.
+        //! For the rotational actuators, the error is given in radians.
+        //! @returns controller error's value in meters or radians
+        virtual float GetError() = 0;
+    };
+
+    using MotorizedJointRequestBus = AZ::EBus<MotorizedJointRequest>;
+} // namespace ROS2

Code/Include/ROS2/Manipulator/MotorizedJointComponent.h
@@ -7,6 +7,7 @@
  */
 #pragma once
 
+#include "MotorizedJointBus.h"
 #include "ROS2/VehicleDynamics/DriveModels/PidConfiguration.h"
 #include <AzCore/Component/Component.h>
 #include <AzCore/Component/TickBus.h>
@@ -24,6 +25,7 @@ namespace ROS2
     class MotorizedJointComponent
         : public AZ::Component
         , public AZ::TickBus::Handler
+        , public MotorizedJointRequestBus::Handler
     {
     public:
         AZ_COMPONENT(MotorizedJointComponent, "{AE9207DB-5B7E-4F70-A7DD-C4EAD8DD9403}", AZ::Component);
@@ -35,16 +37,19 @@ namespace ROS2
         static void Reflect(AZ::ReflectContext* context);
 
         //! Set a setpoint (e.g. desired local position). The controller will follow it.
-        void SetSetpoint(float setpoint);
+        void SetSetpoint(float setpoint) override;
+
+        //! Get a setpoint
+        float GetSetpoint() override;
 
         //! Get current control error. It is the difference between control value and measurement.
         //! When the setpoint is reached this should be close to zero.
         //! @returns control error, in meters for linear joints and in radians for angular joints.
-        float GetError() const;
+        float GetError() override;
 
         //! Get current position from measurement.
         //! @returns current position, in meters for linear joints and radians for angular joints.
-        float GetCurrentPosition() const;
+        float GetCurrentMeasurement() override;
 
         //! Get a degree of freedom direction.
         //! @returns direction of joint movement in global coordinates.

Code/ros2_header_files.cmake
@@ -7,6 +7,7 @@ set(FILES
         Include/ROS2/Frame/NamespaceConfiguration.h
         Include/ROS2/Frame/ROS2FrameComponent.h
         Include/ROS2/Frame/ROS2Transform.h
+        Include/ROS2/Manipulator/MotorizedJointBus.h
         Include/ROS2/Manipulator/MotorizedJointComponent.h
         Include/ROS2/RobotControl/ControlConfiguration.h
         Include/ROS2/RobotControl/ControlSubscriptionHandler.h

commit eacd2e8a7f0b216e427f440d8cdc9418dc8fb9e6

Fix formatting issue introduced in #334
Signed-off-by: Michał Pełka <michalpelka@gmail.com>

Code/Source/Manipulator/MotorizedJointComponent.cpp
@@ -59,9 +59,16 @@ namespace ROS2
                     ->ClassElement(AZ::Edit::ClassElements::EditorData, "MotorizedJointComponent")
                     ->Attribute(AZ::Edit::Attributes::AppearsInAddComponentMenu, AZ_CRC("Game"))
                     ->Attribute(AZ::Edit::Attributes::Category, "MotorizedJointComponent")
-                    ->DataElement(AZ::Edit::UIHandlers::Default, &MotorizedJointComponent::m_jointDir, "Joint Dir.", "Direction of joint in parent's reference frame.")
                     ->DataElement(
-                        AZ::Edit::UIHandlers::Default, &MotorizedJointComponent::m_effortAxis, "Effort Dir.", "Desired direction of force/torque vector that is applied to rigid body.")
+                        AZ::Edit::UIHandlers::Default,
+                        &MotorizedJointComponent::m_jointDir,
+                        "Joint Dir.",
+                        "Direction of joint in parent's reference frame.")
+                    ->DataElement(
+                        AZ::Edit::UIHandlers::Default,
+                        &MotorizedJointComponent::m_effortAxis,
+                        "Effort Dir.",
+                        "Desired direction of force/torque vector that is applied to rigid body.")
 
                     ->DataElement(
                         AZ::Edit::UIHandlers::Default,